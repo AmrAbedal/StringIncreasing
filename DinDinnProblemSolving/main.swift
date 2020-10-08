a5519ed602c7f855383a7af3b28da22d25f4cd19
Amr Abdel-Wahab <amr@orcas.io>
2020-10-08 13:25:53 +0200

    Initial Commit

---
+++ DinDinnProblemSolving/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DinDinnProblemSolving
+//
+//  Created by Amr AbdelWahab on 10/8/20.
+//  Copyright © 2020 Orcas. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
