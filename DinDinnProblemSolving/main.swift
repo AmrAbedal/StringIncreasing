62ba50ebef0c378f1513e3219dc1cc93eedd06cd
Amr Abdel-Wahab <amr@orcas.io>
2020-10-08 15:28:05 +0200

    change method to work with unicode instead of ascii code

--- DinDinnProblemSolving/main.swift
+++ DinDinnProblemSolving/main.swift
@@ -7,22 +7,23 @@
 //
 
 import Foundation
-let zValue: UInt8 = 122
-let aValue: UInt8 = 96
-let ZVlue: UInt8 = 90
-let AValue: UInt8 = 64
+let zValue: UInt32 = 122
+let aValue: UInt32 = 96
+let ZVlue: UInt32 = 90
+let AValue: UInt32 = 64
 
 
-func increaseAsscii(text: String,by number: UInt8) -> String {
+func increase(text: String,by number: UInt8) -> String {
     text.reduce("", {result , character in
-        
-        let number = (character.asciiValue! + number) % (character.isLowercase ? zValue : ZVlue) + (character.isLowercase ? aValue : AValue)
-        let u = UnicodeScalar(number)
+        let x = character.unicodeScalars
+        let value = x[x.startIndex].value + UInt32(number)
+        let number = (value) % (character.isLowercase ? zValue : ZVlue) + (character.isLowercase ? aValue : AValue)
+        let u = UnicodeScalar(UInt8(number))
         return result + "\(u)"
     })
 }
 
 let text = "aBc"
-print(increaseAsscii(text: text, by: 26))
+print(increase(text: text, by: 26))
 //Mark: - disadvantage of using the ASCII value of the letters
 // ASCII is that it only has 94 printable characters — that’s enough for the 26 letters of the basic Latin alphabet in upper and lower case, so we can't use this function with other languages or an emojies .
