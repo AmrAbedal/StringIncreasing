0f4ae821b978a0812a040324947b5eae2188f5c7
Amr Abdel-Wahab <amr@orcas.io>
2020-10-08 15:40:10 +0200

    indent code and change method name to  increment String by

--- DinDinnProblemSolving/main.swift
+++ DinDinnProblemSolving/main.swift
@@ -13,20 +13,20 @@ let ZVlue: UInt32 = 90
 let AValue: UInt32 = 64
 
 
-func increase(text: String,by number: UInt8) -> String {
+func increment(text: String,by number: UInt8) -> String {
     text.reduce("", {result , character in
         if character.isASCII {
-        let unicodeScalars = character.unicodeScalars
-        let value = unicodeScalars[unicodeScalars.startIndex].value + UInt32(number)
-        let number = (value) % (character.isLowercase ? zValue : ZVlue) + (character.isLowercase ? aValue : AValue)
-        let u = UnicodeScalar(UInt8(number))
-            return result + "\(u)" } else {
-           return result + "\(character)"
+            let unicodeScalars = character.unicodeScalars
+            let characterUniCodeValueAfterIncrement = unicodeScalars[unicodeScalars.startIndex].value + UInt32(number)
+            let number = (characterUniCodeValueAfterIncrement) % (character.isLowercase ? zValue : ZVlue) + (character.isLowercase ? aValue : AValue)
+            let newCharacter = UnicodeScalar(UInt8(number))
+            return result + "\(newCharacter)" } else {
+            return result + "\(character)"
         }
     })
 }
 
 let text = "a😃B😎c"
-print(increase(text: text, by: 26))
+print(increment(text: text, by: 26))
 //Mark: - disadvantage of using the ASCII value of the letters
 // ASCII is that it only has 94 printable characters — that’s enough for the 26 letters of the basic Latin alphabet in upper and lower case, so we can't use this it with other languages or an emojies .
